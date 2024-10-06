fec3cc0566f4b4540f1c7983ae2303797b9e8762
ardan1996 <107191574+ardan1996@users.noreply.github.com>
2024-10-06 21:32:31 +0300

    add file short_phrase_test.py

---
+++ short_phrase_test.py
@@ -0,0 +1,5 @@
+class TestExample:
+    phrase_check = input('Set a phrase: ')
+    a = len(phrase_check)
+    number_of_characters = 15
+    assert a <= number_of_characters, f"The phrase must be shorter than {number_of_characters} characters"
